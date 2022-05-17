a2f36e87558c1341dd29bdec8e550d8ad295587a
Erik <Erik.quintana@icloud.com>
2022-05-17 16:29:24 -0600

    made calcutes Fare Price

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ function distanceTravelledInFeet(startingBlock, endBlock) {
     const feetTravled = (startingBlock - endBlock) * 264 
     return Math.abs(feetTravled)
 }
+
+function calculatesFarePrice(start, destination){
+    let fare
+    // function distanceTravelledInFeet(startingBlock, endBlock) {
+    //     const feetTravled = (startingBlock - endBlock) * 264 
+    //     const result = Math.abs(feetTravled)
+    //     return result
+    // }
+    if(distanceTravelledInFeet(start, destination) < 400){
+        return fare = 0
+    } else if (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) <= 2000 ){
+        return (distanceTravelledInFeet(start, destination)-400) * .02 
+    } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500 ){
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
