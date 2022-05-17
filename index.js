ac5982db40702836f781162f9767513b12009285
Lior <levydlior@gmail.com>
2022-05-17 15:22:04 -0600

    distance from hq function

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(startingBlock){
+    let headquarters = 42 
+    const distance = headquarters - startingBlock;
+    return Math.abs(distance)
+}
